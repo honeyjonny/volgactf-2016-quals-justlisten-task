d4505382798c4d6749a436186fb168493f050c2e
Тихонов Денис <tikhonov@kg.ru>
2016-03-21 22:14:21 +0400

    Base for vork with mongo

--- handlers.py
+++ handlers.py
@@ -2,17 +2,26 @@ import tornado.web
 import tornado.escape
 
 
-class MainHandler(tornado.web.RequestHandler):
+class Basehandler(tornado.web.RequestHandler):
+	async def find_users(self):
+		return [u for u in self.application.db.users.find({}, {"_id": 0 })]
+
+	async def create_user(self, user):
+		return self.application.db.users.save(user)
+
+	async def find_user_byname(self, name):
+		return self.application.db.users.find( {"name":name} )
+
+
+
+class MainHandler(Basehandler):
 	async def get(self):
 		resp = await self.find_users()
 
 		self.write({"users": resp})
 
-	async def find_users(self):
-		return [u for u in self.application.db.users.find({}, {"_id": 0 })]
 
-
-class RegisterHandler(tornado.web.RequestHandler):
+class RegisterHandler(Basehandler):
 	async def post(self):
 
 		print(self.request.body)
@@ -22,11 +31,16 @@ class RegisterHandler(tornado.web.RequestHandler):
 		username = body["username"]
 		password = body["password"]
 
+		cursor = await self.find_user_byname(username)
+		isExist = cursor.count()
+
+		if isExist > 0:
+			self.write({"error": "user already exists"})
+			self.flush()
+			return
+
 		usr = {"name":username, "pass": password}
 
 		uid = await self.create_user(usr)
 
 		self.write({"userId": str(uid)})
-
-	async def create_user(self, user):
-		return self.application.db.users.save(user)
